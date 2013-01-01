1b7fe6d15e7ee6d08c81b3488635b1ead91bfe95
Sergej Kurakin <sergej@kurakin.info>
2013-01-01 19:23:33 +0200

    Add nginx main config files as ERB templates

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -3,6 +3,56 @@ class nginx {
     ensure => present,
   }
 
+  file { '/etc/nginx/nginx.conf':
+    ensure => present,
+    source => template('nginx/nginx.conf.erb'),
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+
+  file { '/etc/nginx/fastcgi_params':
+    ensure => present,
+    source => template('nginx/fastcgi_params.erb'),
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+
+  file { '/etc/nginx/mime.types':
+    ensure => present,
+    source => template('nginx/mime.types.erb'),
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+
+  file { '/etc/nginx/scgi_params':
+    ensure => present,
+    source => template('nginx/scgi_params.erb'),
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+
+  file { '/etc/nginx/uwsgi_params':
+    ensure => present,
+    source => template('nginx/uwsgi_params.erb'),
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+
   service { 'nginx':
     ensure => running,
     hasstatus => true,
